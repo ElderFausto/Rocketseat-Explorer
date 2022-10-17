31a6b63cbab3b32c6af8c39bb00f202640193fb7
Elder <elderfavsto@gmail.com>
2022-10-17 17:22:04 -0300

    initial stage 05

---
+++ Stage 05 - Avançando no JavaScript/Jogo da adivinhação/script.js
@@ -0,0 +1,20 @@
+const randomNumber = Math.round(Math.random() * 10);
+let tentativas = 1;
+
+const handleClick = (event) => {
+  event.preventDefault();
+
+  const inputNumber = document.querySelector(".inputNumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    document.querySelector(".screen1").classList.add("hide");
+    document.querySelector(".screen2").classList.remove("hide");
+
+    document
+    .querySelector('.screen2 h2')
+    .innterText = `acertou em ${tentativas} tentativas`
+  }
+
+  tentativas++;
+}
+
