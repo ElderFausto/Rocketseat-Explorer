106123bc47a9e3d88f2183103b722ca6a00e929a
Elder <elderfavsto@gmail.com>
2022-10-18 22:56:01 -0300

    projeto imc

--- Stage 05 - Avançando no JavaScript/Jogo da adivinhação/script.js
+++ Stage 05 - Avançando no JavaScript/Jogo da adivinhação/script.js
@@ -1,20 +1,36 @@
+const btnTry = document.querySelector(".btnTry");
+const btnReset = document.querySelector(".btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
 const randomNumber = Math.round(Math.random() * 10);
 let tentativas = 1;
 
-const handleClick = (event) => {
+
+// funções
+const handleTryClick = (event) => {
   event.preventDefault();
 
   const inputNumber = document.querySelector(".inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
+    toggleScreen();
 
-    document
-    .querySelector('.screen2 h2')
-    .innterText = `acertou em ${tentativas} tentativas`
+    screen2.querySelector("h2").innerText = `acertou em ${tentativas} tentativas`;
   }
 
+  inputNumber.value = "";
   tentativas++;
-}
+};
 
+const handleResetClick = () => {
+  toggleScreen();
+  tentativas = 1;
+};
+
+const toggleScreen = () => {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+};
+
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
