71b798d9e708846674b5758150a1cac46ef687a8
Elder <elderfavsto@gmail.com>
2023-01-03 17:03:07 -0300

    metodos nodejs

--- Stage 08 - Backend/API/src/server.js
+++ Stage 08 - Backend/API/src/server.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.get("/message/:id/:user", (req, res) => {
+  const { id, user } = req.params;
+
+  res.send(`
+    Id da mensagem ${id}.
+    Para o usuario: ${user}
+  `);
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+});
+
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
