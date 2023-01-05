7f2fd4a4cc18f715deaf9bfc7073796ff05f7f1b
Elder <elderfavsto@gmail.com>
2023-01-05 17:12:14 -0300

    ajustes imnsonia

--- Stage 08 - Backend/API/src/controllers/UsersControllers.js
+++ Stage 08 - Backend/API/src/controllers/UsersControllers.js
@@ -1,8 +1,15 @@
+const AppError = require("../utils/AppError")
+
 class UsersControllers {
   
     create(request, response) {
-      const { name, email, password } = request.body;
-      response.json({ name, email, password });
+      const { name, email, password } = request.bodyaaaa;
+
+      if(!name) {
+        throw new AppError("Nome é obrigatorio")
+      }
+
+      response.status(201).json({ name, email, password });
     }
 }
 
--- Stage 08 - Backend/API/src/routes/users.routes.js
+++ Stage 08 - Backend/API/src/routes/users.routes.js
@@ -1,10 +1,21 @@
 const { Router } = require("express")
 
-const UsersController = require("../controllers/UsersController")
+const UsersController = require("../controllers/UsersControllers")
+
 const userRoutes = Router()
+
+function myMiddleware(request, response, next) {
+  console.log('Viva Cristo Rei');
+  
+  // if(request.body.isAdmin) {
+  //   return response.json({message: "usuario nao autorizado"})
+  // }
+
+  next();
+}
 
 const usersController = new UsersController();
 
-userRoutes.post("/", usersController.create);
+userRoutes.post("/", myMiddleware, usersController.create);
 
 module.exports = userRoutes;
