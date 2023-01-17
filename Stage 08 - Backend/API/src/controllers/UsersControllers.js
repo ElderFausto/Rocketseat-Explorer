09d264e6001511476f11c1d47b29fcd949c3bb94
Elder <elderfavsto@gmail.com>
2023-01-17 16:25:11 -0300

    bd

--- Stage 08 - Backend/API/src/controllers/UsersControllers.js
+++ Stage 08 - Backend/API/src/controllers/UsersControllers.js
@@ -1,16 +1,14 @@
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+
+const sqliteConnection = require('../database/sqlite')
 
 class UsersControllers {
-  
-    create(request, response) {
-      const { name, email, password } = request.bodyaaaa;
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
-      if(!name) {
-        throw new AppError("Nome é obrigatorio")
-      }
-
-      response.status(201).json({ name, email, password });
-    }
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users")
+  }
 }
 
 module.exports = UsersControllers;
--- Stage 08 - Backend/API/src/routes/users.routes.js
+++ Stage 08 - Backend/API/src/routes/users.routes.js
@@ -4,18 +4,9 @@ const UsersController = require("../controllers/UsersControllers")
 
 const userRoutes = Router()
 
-function myMiddleware(request, response, next) {
-  console.log('Viva Cristo Rei');
-  
-  // if(request.body.isAdmin) {
-  //   return response.json({message: "usuario nao autorizado"})
-  // }
-
-  next();
-}
 
 const usersController = new UsersController();
 
-userRoutes.post("/", myMiddleware, usersController.create);
+userRoutes.post("/", usersController.create);
 
 module.exports = userRoutes;
