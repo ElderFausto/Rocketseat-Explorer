471657f9b8e6f5fed8c20b079f3bd45b0b36d7f4
Elder <elderfavsto@gmail.com>
2023-02-28 18:13:51 -0300

    middlewares and config insomnia

--- Stage 08 - Backend/src/server.js
+++ Stage 08 - Backend/src/server.js
@@ -1,23 +1,32 @@
-const express = require('express')
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
+const express = require("express");
+
+const routes = require("./routes")
+
 const app = express();
+app.use(express.json());
+
+app.use(routes)
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "internal server error"
+  })
+})
+
 const PORT = 3333;
-
-app.get('/message/:id/:user', (req, res) => {
-  const {id, user} = req.params
-
-  res.send(`
-    id da mensagem:  ${id},
-    para o usuario: ${user}
-    `)
-})
-
-//query param
-app.get("/users", (req, res) => {
-  const {page, limit} = req.query
-  // http://localhost:3333/users?page=5&limit=10
-  res.send(`Pagina: ${page}, mostrar: ${limit}`)
-})
-
 app.listen(PORT, () => {
   console.log(`o servidor esta rodando na porta ${PORT}`);
-})
+});
